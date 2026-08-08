99438b353c83ed136852aa40e2d8523e2ac76b15
Famura71 <famura5523@gmail.com>
2026-08-08 21:39:43 +0300

    Hermes push 2

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"Main", "Server", "Database", "Transfer", "Fafnir"})
-@EntityScan(basePackages = {"Transfer.Hibernate"})
-@EnableJpaRepositories(basePackages = {"Transfer.Hibernate"})
+@EntityScan(basePackages = {"Transfer.Hibernate", "Server.Kahvehane"})
+@EnableJpaRepositories(basePackages = {"Transfer.Hibernate", "Server.Kahvehane"})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
